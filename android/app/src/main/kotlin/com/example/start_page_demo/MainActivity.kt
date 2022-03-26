ee6b4a7466c822716fd9bd7ebaf6733be04b5e03
vbprudnikov <prudnikov.bgu@mail.ru>
2022-03-26 16:49:21 +0500

    Создан макет страницы авторизации

---
+++ android/app/src/main/kotlin/com/example/start_page_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.start_page_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+
+
+    const borderStyle = OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(15)),
+        borderSide: BorderSide(
+          color: const Color (0xFF000000),
+          width: 2,
+        )
+
+    );
+
+    const linkStyle = TextStyle(
+      fontSize: 18,
+      fontWeight: FontWeight.bold,
+      color: Color(0xFF1A4F2F),
+    );
+
+    return MaterialApp(
+
+      home: Scaffold(
+
+          body: Column(
+            children:  [
+              SizedBox(height: 45),
+
+                  Center(
+                    child: SizedBox(
+                      width: 97,
+                      height: 98,
+                      child: Placeholder()),
+                  ),
+              SizedBox(height: 20),
+              Text('Введите логин (10 цифр)'),
+              SizedBox(height: 30),
+              SizedBox(
+                width: 250,
+                height: 50,
+                child: TextField(
+                  decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Color(0xFFC4C4C4),
+                      labelText: 'Телефон',
+                      enabledBorder: borderStyle,
+                    focusedBorder: borderStyle,
+
+                ),
+              ),
+              ),
+              SizedBox(height: 30),
+              SizedBox(
+                width: 250,
+                height: 50,
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Color(0xFFC4C4C4),
+                    labelText: 'Пароль',
+                    enabledBorder: borderStyle,
+                    focusedBorder: borderStyle,
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+              SizedBox(
+                width: 175,
+                height: 45,
+                child: ElevatedButton(onPressed: () {},
+                    child: Text('Войти'),
+                  style: ElevatedButton.styleFrom(
+                      primary: Color(0xFF1A4F2F),
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15),
+                      ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              InkWell(
+                  child: Text('Зарегистрироваться',
+                      style: linkStyle),
+                  onTap: () {}),
+              InkWell(
+                  child: Text('Забыли пароль',
+                      style: linkStyle),
+                  onTap: () {}),
+
+
+            ],
+          ),
+      ),
+    );
+  }
+}
+
